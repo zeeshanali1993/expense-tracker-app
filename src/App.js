50c8abab56c922851416668309270468a3f78f51
Zeeshan Ali <zeeshanali93@outlook.com>
2020-10-01 23:34:20 +0500

    add basic css and initial components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './HEADER';
 import './App.css';
 
 function App()
 {
   return (
     <div className="App">
-      {/* <header className="App-header">
-
-      </header> */}
+      <Header />
     </div>
   );
 }
---
+++ src/HEADER.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './HEADER.css'
+export default function Header(props)
+{
+    return (
+        <div className='headerDiv'>
+            <h1>Expense Tracker</h1>
+            <h2>Current Balance:</h2>
+        </div>
+    );
+}
