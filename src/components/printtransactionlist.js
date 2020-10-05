0a9a8b3663cd20f942efaa717851b7aa4b9dc5bd
Zeeshan Ali <zeeshanali93@outlook.com>
2020-10-05 14:38:51 +0500

    transaction deletion successful but state not changing

--- src/components/printtransactionlist.js
+++ src/components/printtransactionlist.js
@@ -5,8 +5,13 @@ export const Printtransactions = ({ x }) =>
 {
     const sign = x.amount < 0 ? '-' : '+';
     return (
-        < li className={x.amount >= 0 ? 'plus' : 'minus'}>
-            {x.text}<span span > {sign}${Math.abs(x.amount)}</span> <button className='delete-btn'>x</button>
+        < li id={x.id} className={x.amount >= 0 ? 'plus' : 'minus'}>
+            {x.text}<span span > {sign}${Math.abs(x.amount)}</span> <button onClick={() =>
+            {
+                const elementId = document.getElementById(x.id);
+                elementId.remove();
+            }
+            } className='delete-btn'>x</button>
         </li >
     );
 }
